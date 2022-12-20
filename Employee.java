727ea110f7c4edfdc5b401ee86b62910b2a6dd57
zilllionsoftech <zillionsoftech@gmail.com>
2022-12-20 22:28:21 +0530

    new emplyee class added

---
+++ Employee.java
@@ -0,0 +1,9 @@
+class Employee{
+    public Employee(){
+        System.out.println("Working fine");
+    }
+
+    public static void main(String[] args){
+        Employee obj=new Employee();
+    }
+}
